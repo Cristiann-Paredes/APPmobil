239a955007fda1e8418c9c7020a4671b01a79184
cristian <paredes30@hotmail.es>
2024-11-04 21:54:22 -0500

    icono assets

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,17 +2,23 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'primera_app_ionic_24b',
+  appName: 'CristianAPP',
   webDir: 'www',
-  bundledWebRuntime: false,
   plugins: {
     SplashScreen: {
-      launchShowDuration: 3000,  // Duración del splash screen en milisegundos
+      launchShowDuration: 0,
       launchAutoHide: true,
-      backgroundColor: '#ffffff', // Color de fondo del splash screen
-      androidScaleType: 'CENTER_CROP',
-      showSpinner: true, // Muestra un spinner de carga
-      spinnerColor: '#999999',
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: false,
+      splashImmersive: false,
+      layoutName: "launch_screen",
+      useDialog: false,
     },
   },
 };
